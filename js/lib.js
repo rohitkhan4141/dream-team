1ec67f30f4faa83bcfaaf595c5b2cf8fa7a89df9
rohitkhan4141 <rohitkhan4141@gmail.com>
2022-08-22 12:24:50 +0600

    javascript selected players added

--- js/lib.js
+++ js/lib.js
@@ -1,1 +1,20 @@
-console.log("hello from lib.js");
+/* geeting any element data from the dom*/
+function gettingDataFromDom(selectorID, isInput) {
+  const elementSelector = document.getElementById(selectorID);
+  let data;
+  if (isInput === true) {
+    data = parseInt(elementSelector.value);
+  } else {
+    data = parseInt(elementSelector.innerText);
+  }
+  return data;
+}
+/* setting any element data into the dom*/
+function settingDataIntoDom(selectorID, isInput, value) {
+  const elementSelector = document.getElementById(selectorID);
+  if (isInput === true) {
+    elementSelector.value = value;
+  } else {
+    elementSelector.innerText = value;
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log("hello from script.js");
+const selectBtnList = document.querySelectorAll(".btn-select");
+const playerList = document.querySelectorAll(".player-name");
+
+const selectedPlayerList = document.getElementById("selected-players");
+
+let count = 0;
+
+for (let i = 0; i < selectBtnList.length; i++) {
+  selectBtnList[i].addEventListener("click", function () {
+    count += 1;
+    if (count <= 5) {
+      selectedPlayerList.innerHTML += `
+        <li class="py-2">${playerList[i].innerText}</li>
+        `;
+      selectBtnList[i].disabled = true;
+    } else {
+      alert("You Cant Add More Than Five Players");
+    }
+  });
+}
